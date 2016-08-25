a5010251cf28b9dd05a54d6000efaa3d889adc34
shift02 <shift02ss@gmail.com>
2016-08-25 16:24:50 +0900

    追加：感圧板

--- src/main/java/shift/sextiarysector3/SSBlocks.java
+++ src/main/java/shift/sextiarysector3/SSBlocks.java
@@ -1,17 +1,40 @@
 package shift.sextiarysector3;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import shift.sextiarysector3.block.BlockSSOre;
+import shift.sextiarysector3.block.BlockSSPressurePlate;
+import shift.sextiarysector3.block.BlockSSPressurePlate.Sensitivity;
 import shift.sextiarysector3.util.UtilRegistry;
 
 public class SSBlocks {
 
+	//Core
+	/** 銅の感圧板 */
+	public static Block copperPressurePlate;
+	/** 銀の感圧板 */
+	public static Block silverPressurePlate;
+	/** オリハルコンの感圧板 */
+	public static Block orichalcumPressurePlate;
+
+	//鉱石
 	public static Block copperOre;
 	public static Block silverOre;
 	public static Block orichalcumOre;
 
 	public static void initBlock() {
 
+		//Core
+		copperPressurePlate = new BlockSSPressurePlate(Material.IRON, Sensitivity.AGEABLE).setUnlocalizedName("ss.copper_pressure_plate");
+		UtilRegistry.registerNormalBlock(copperPressurePlate, "CopperPressurePlate", "copper_pressure_plate");
+
+		silverPressurePlate = new BlockSSPressurePlate(Material.IRON, Sensitivity.ENEMY).setUnlocalizedName("ss.silver_pressure_plate");
+		UtilRegistry.registerNormalBlock(silverPressurePlate, "SilverPressurePlate", "silver_pressure_plate");
+
+		orichalcumPressurePlate = new BlockSSPressurePlate(Material.IRON, Sensitivity.XPORB).setUnlocalizedName("ss.orichalcum_pressure_plate");
+		UtilRegistry.registerNormalBlock(orichalcumPressurePlate, "OrichalcumPressurePlate", "orichalcum_pressure_plate");
+
+		//鉱石
 		copperOre = new BlockSSOre().setUnlocalizedName("ss.copper_ore");
 		UtilRegistry.registerNormalBlock(copperOre, "CopperOre", "ore/copper_ore");
 
