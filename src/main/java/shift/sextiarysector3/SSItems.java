aa9f8a30c01da2b41bf00b41aa900b73f5eefbf3
shift02 <shift02ss@gmail.com>
2016-08-27 22:57:35 +0900

    追加：鉱石

--- src/main/java/shift/sextiarysector3/SSItems.java
+++ src/main/java/shift/sextiarysector3/SSItems.java
@@ -7,11 +7,19 @@ import shift.sextiarysector3.util.UtilRegistry;
 public class SSItems {
 
 	public static Item copperIngot;
+	public static Item silverIngot;
+	public static Item orichalcumGem;
 
 	public static void initItem() {
 
 		copperIngot = new ItemIngot().setUnlocalizedName("ss.copper_ingot");
 		UtilRegistry.registerNormalItem(copperIngot, "CopperIngot", "ingot/copper_ingot");
+
+		silverIngot = new ItemIngot().setUnlocalizedName("ss.silver_ingot");
+		UtilRegistry.registerNormalItem(silverIngot, "SilverIngot", "ingot/silver_ingot");
+
+		orichalcumGem = new ItemIngot().setUnlocalizedName("ss.orichalcum_gem");
+		UtilRegistry.registerNormalItem(orichalcumGem, "OrichalcumGem", "gem/orichalcum_gem");
 
 	}
 }
