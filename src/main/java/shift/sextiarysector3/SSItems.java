aff6e03057aee0cd0908b6ab3ab27cb7d17cf0ce
shift02 <shift02ss@gmail.com>
2016-09-03 14:02:33 +0900

    追加：樹液ボトル

--- src/main/java/shift/sextiarysector3/SSItems.java
+++ src/main/java/shift/sextiarysector3/SSItems.java
@@ -5,6 +5,7 @@ import shift.sextiarysector3.api.SextiarySectorAPI;
 import shift.sextiarysector3.item.ItemIngot;
 import shift.sextiarysector3.item.ItemSSBase;
 import shift.sextiarysector3.item.ItemSSShield;
+import shift.sextiarysector3.item.ItemSapBottle;
 import shift.sextiarysector3.renderer.RendererShield;
 import shift.sextiarysector3.tileentity.TileEntityShield;
 import shift.sextiarysector3.util.UtilRegistry;
@@ -12,6 +13,9 @@ import shift.sextiarysector3.util.UtilRegistry;
 public class SSItems {
 
 	public static Item plasticShield;
+
+	public static Item sapBottle;
+	public static Item rubberBottle;
 
 	public static Item copperIngot;
 	public static Item silverIngot;
@@ -24,6 +28,14 @@ public class SSItems {
 		plasticShield = new ItemSSShield().setUnlocalizedName("ss.plastic_shield");
 		UtilRegistry.registerCustomItem(plasticShield, "PlasticShield", "plastic_shield", TileEntityShield.class, new RendererShield());
 
+		//林業
+		sapBottle = new ItemSapBottle().setUnlocalizedName("ss.sap_bottle");
+		UtilRegistry.registerNormalItem(sapBottle, "SapBottle", "fluid/sap_bottle");
+
+		rubberBottle = new ItemSapBottle().setUnlocalizedName("ss.rubber_bottle");
+		UtilRegistry.registerNormalItem(rubberBottle, "RubberBottle", "fluid/rubber_bottle");
+
+		//鉱業
 		copperIngot = new ItemIngot().setUnlocalizedName("ss.copper_ingot");
 		UtilRegistry.registerNormalItem(copperIngot, "CopperIngot", "ingot/copper_ingot");
 
@@ -33,6 +45,7 @@ public class SSItems {
 		orichalcumGem = new ItemIngot().setUnlocalizedName("ss.orichalcum_gem");
 		UtilRegistry.registerNormalItem(orichalcumGem, "OrichalcumGem", "gem/orichalcum_gem");
 
+		//工業
 		plastic = new ItemSSBase().setUnlocalizedName("ss.plastic").setCreativeTab(SextiarySectorAPI.TabSSIndustry);
 		UtilRegistry.registerNormalItem(plastic, "Plastic", "plastic");
 
---
+++ src/main/java/shift/sextiarysector3/item/ItemSapBottle.java
@@ -0,0 +1,12 @@
+package shift.sextiarysector3.item;
+
+import shift.sextiarysector3.api.SextiarySectorAPI;
+
+public class ItemSapBottle extends ItemSSBase {
+
+	public ItemSapBottle() {
+		this.setCreativeTab(SextiarySectorAPI.TabSSForestry);
+		this.setMaxStackSize(1);
+	}
+
+}
