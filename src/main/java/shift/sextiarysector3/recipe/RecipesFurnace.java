49e0894b6d10957689c7bf19bf3fe8cc5653bfc3
shift02 <shift02ss@gmail.com>
2016-09-07 22:51:04 +0900

    追加：鉱石を焼く処理

--- src/main/java/shift/sextiarysector3/recipe/RecipesFurnace.java
+++ src/main/java/shift/sextiarysector3/recipe/RecipesFurnace.java
@@ -2,6 +2,7 @@ package shift.sextiarysector3.recipe;
 
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import shift.sextiarysector3.SSBlocks;
 import shift.sextiarysector3.SSItems;
 
 public class RecipesFurnace {
@@ -9,6 +10,10 @@ public class RecipesFurnace {
 	public static void addRecipes() {
 
 		GameRegistry.addSmelting(new ItemStack(SSItems.sapBottle), new ItemStack(SSItems.plastic), 0.1f);
+
+		GameRegistry.addSmelting(new ItemStack(SSBlocks.copperOre), new ItemStack(SSItems.copperIngot), 0.1f);
+		GameRegistry.addSmelting(new ItemStack(SSBlocks.silverOre), new ItemStack(SSItems.silverIngot), 0.1f);
+		GameRegistry.addSmelting(new ItemStack(SSBlocks.orichalcumOre), new ItemStack(SSItems.orichalcumGem), 0.1f);
 
 	}
 }
