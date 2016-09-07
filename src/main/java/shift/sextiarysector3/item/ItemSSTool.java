dac7d6c96f7d6b491b766f3dc5b3f6cad3f8080e
shift02 <shift02ss@gmail.com>
2016-09-07 23:04:31 +0900

    追加：道具

---
+++ src/main/java/shift/sextiarysector3/item/ItemSSTool.java
@@ -0,0 +1,14 @@
+package shift.sextiarysector3.item;
+
+import java.util.Set;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemTool;
+
+public class ItemSSTool extends ItemTool {
+
+	protected ItemSSTool(ToolMaterial materialIn, Set<Block> effectiveBlocksIn) {
+		super(materialIn, effectiveBlocksIn);
+	}
+
+}
