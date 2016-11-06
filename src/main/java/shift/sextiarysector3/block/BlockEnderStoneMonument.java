155c1f257e852c6d7121ad955220bedadae77d27
shift02 <shift02ss@gmail.com>
2016-11-06 17:18:12 +0900

    追加：エンダー石碑の向き

--- src/main/java/shift/sextiarysector3/block/BlockEnderStoneMonument.java
+++ src/main/java/shift/sextiarysector3/block/BlockEnderStoneMonument.java
@@ -3,20 +3,21 @@ package shift.sextiarysector3.block;
 import java.util.List;
 import java.util.Random;
 
-import javax.annotation.Nullable;
-
+import net.minecraft.block.BlockHorizontal;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityHanging;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.item.EntityItemFrame;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -29,82 +30,18 @@ public class BlockEnderStoneMonument extends BlockSSBase {
 
     protected static final AxisAlignedBB FRAME_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.00D, 1.0D);
 
+    public static final PropertyDirection FACING = BlockHorizontal.FACING;
+
     public BlockEnderStoneMonument() {
         super(Material.ROCK);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
         //this.setTickRandomly(true);
     }
 
-    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem,
-            EnumFacing side, float hitX, float hitY, float hitZ) {
-
-        if (this.onSilver(worldIn, pos)) {
-
-            EntityItem eItem = this.getOnSilver(worldIn, pos);
-            eItem.getEntityItem().stackSize = 1;
-            //eItem.motionY += 0.2;
-            if (worldIn.isRemote) return true;
-
-            eItem.setAgeToCreativeDespawnTime();
-            eItem.setNoPickupDelay();
-
-            return true;
-
-        } else {
-
-            if (heldItem == null) return true;
-
-            if (worldIn.isRemote) return true;
-            EntityItem eItem = new EntityItem(worldIn, pos.getX() + 0.5, pos.getY() + 1.3, pos.getZ() + 0.5);
-            eItem.motionX = 0;
-            eItem.motionY = 0;
-            eItem.motionZ = 0;
-            eItem.setEntityItemStack(heldItem.copy());
-            eItem.setNoDespawn();
-            eItem.setInfinitePickupDelay();
-
-            eItem.getEntityData().setBoolean("silver", true);
-
-            worldIn.spawnEntityInWorld(eItem);
-
-            return true;
-
-        }
-
-    }
-
-    private boolean onSilver(World worldIn, BlockPos pos) {
-
-        AxisAlignedBB axisalignedbb = FRAME_AABB.offset(pos.up());
-
-        List<? extends EntityItem> list = worldIn.getEntitiesWithinAABB(EntityItem.class, axisalignedbb);
-
-        for (EntityItem eI : list) {
-
-            if (eI.getEntityData().hasKey("silver")) return true;
-
-        }
-
-        return false;
-
-    }
-
-    private EntityItem getOnSilver(World worldIn, BlockPos pos) {
-
-        AxisAlignedBB axisalignedbb = FRAME_AABB.offset(pos.up());
-
-        List<? extends EntityItem> list = worldIn.getEntitiesWithinAABB(EntityItem.class, axisalignedbb);
-
-        for (EntityItem eI : list) {
-
-            if (eI.getEntityData().hasKey("silver")) return eI;
-
-        }
-
-        return null;
-
-    }
-
+    //動作系
     public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+
+        worldIn.setBlockState(pos, state.withProperty(FACING, placer.getHorizontalFacing().getOpposite()), 2);
 
         worldIn.scheduleBlockUpdate(pos, state.getBlock(), this.tickRate(worldIn), 0);
 
@@ -203,6 +140,63 @@ public class BlockEnderStoneMonument extends BlockSSBase {
             setEnder(worldIn, pos.offset(f), worldIn.getBlockState(pos), rand, f);
         }
 
+    }
+
+    //石の向き
+    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+        this.setDefaultFacing(worldIn, pos, state);
+    }
+
+    private void setDefaultFacing(World worldIn, BlockPos pos, IBlockState state) {
+        if (!worldIn.isRemote) {
+            IBlockState iblockstate = worldIn.getBlockState(pos.north());
+            IBlockState iblockstate1 = worldIn.getBlockState(pos.south());
+            IBlockState iblockstate2 = worldIn.getBlockState(pos.west());
+            IBlockState iblockstate3 = worldIn.getBlockState(pos.east());
+            EnumFacing enumfacing = (EnumFacing) state.getValue(FACING);
+
+            if (enumfacing == EnumFacing.NORTH && iblockstate.isFullBlock() && !iblockstate1.isFullBlock()) {
+                enumfacing = EnumFacing.SOUTH;
+            } else if (enumfacing == EnumFacing.SOUTH && iblockstate1.isFullBlock() && !iblockstate.isFullBlock()) {
+                enumfacing = EnumFacing.NORTH;
+            } else if (enumfacing == EnumFacing.WEST && iblockstate2.isFullBlock() && !iblockstate3.isFullBlock()) {
+                enumfacing = EnumFacing.EAST;
+            } else if (enumfacing == EnumFacing.EAST && iblockstate3.isFullBlock() && !iblockstate2.isFullBlock()) {
+                enumfacing = EnumFacing.WEST;
+            }
+
+            worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing), 2);
+        }
+    }
+
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+    }
+
+    public IBlockState getStateFromMeta(int meta) {
+        EnumFacing enumfacing = EnumFacing.getFront(meta);
+
+        if (enumfacing.getAxis() == EnumFacing.Axis.Y) {
+            enumfacing = EnumFacing.NORTH;
+        }
+
+        return this.getDefaultState().withProperty(FACING, enumfacing);
+    }
+
+    public int getMetaFromState(IBlockState state) {
+        return ((EnumFacing) state.getValue(FACING)).getIndex();
+    }
+
+    public IBlockState withRotation(IBlockState state, Rotation rot) {
+        return state.withProperty(FACING, rot.rotate((EnumFacing) state.getValue(FACING)));
+    }
+
+    public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
+        return state.withRotation(mirrorIn.toRotation((EnumFacing) state.getValue(FACING)));
+    }
+
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[] { FACING });
     }
 
 }
