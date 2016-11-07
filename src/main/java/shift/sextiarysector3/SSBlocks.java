136486d8eceb136f7aa2d61ecd8ce353335a6144
shift02 <shift02ss@gmail.com>
2016-11-07 09:58:47 +0900

    追加：エンダー石碑の土台

--- src/main/java/shift/sextiarysector3/SSBlocks.java
+++ src/main/java/shift/sextiarysector3/SSBlocks.java
@@ -22,129 +22,133 @@ import shift.sextiarysector3.util.UtilRegistry;
 
 public class SSBlocks {
 
-	//Core
-	public static Block lapisPressurePlate;
+    //Core
+    public static Block lapisPressurePlate;
 
-	/** 銅の感圧板 */
-	public static Block copperPressurePlate;
-	/** 銀の感圧板 */
-	public static Block silverPressurePlate;
-	/** オリハルコンの感圧板 */
-	public static Block orichalcumPressurePlate;
+    /** 銅の感圧板 */
+    public static Block copperPressurePlate;
+    /** 銀の感圧板 */
+    public static Block silverPressurePlate;
+    /** オリハルコンの感圧板 */
+    public static Block orichalcumPressurePlate;
 
-	public static Block sanctuary;
+    public static Block sanctuary;
 
-	public static Block enderStone;
+    public static Block enderStone;
+    public static Block enderStoneFoundation;
 
-	public static Block rubberSapling;
-	public static Block rubberLog;
-	public static Block rubberLeaves;
+    public static Block rubberSapling;
+    public static Block rubberLog;
+    public static Block rubberLeaves;
 
-	public static Block spile;
+    public static Block spile;
 
-	//樹液
-	public static Block sapCauldron;
-	public static Block rubberCauldron;
+    //樹液
+    public static Block sapCauldron;
+    public static Block rubberCauldron;
 
-	//鉱石
-	public static Block copperOre;
-	public static Block silverOre;
-	public static Block orichalcumOre;
+    //鉱石
+    public static Block copperOre;
+    public static Block silverOre;
+    public static Block orichalcumOre;
 
-	public static Block coalLargeOre;
-	public static Block ironLargeOre;
-	public static Block goldLargeOre;
-	//public static Block diamondLargeOre;
+    public static Block coalLargeOre;
+    public static Block ironLargeOre;
+    public static Block goldLargeOre;
+    //public static Block diamondLargeOre;
 
-	public static Block copperLargeOre;
-	public static Block silverLargeOre;
-	//public static Block orichalcumLargeOre;
+    public static Block copperLargeOre;
+    public static Block silverLargeOre;
+    //public static Block orichalcumLargeOre;
 
-	public static Block copperBlock;
-	public static Block silverBlock;
-	public static Block orichalcumBlock;
+    public static Block copperBlock;
+    public static Block silverBlock;
+    public static Block orichalcumBlock;
 
-	public static void initBlock() {
+    public static void initBlock() {
 
-		//Core
-		lapisPressurePlate = new BlockSSPressurePlate(Material.IRON, Sensitivity.XPORB).setUnlocalizedName("ss.lapis_pressure_plate");
-		UtilRegistry.registerNormalBlock(lapisPressurePlate, "LapisPressurePlate", "lapis_pressure_plate");
+        //Core
+        lapisPressurePlate = new BlockSSPressurePlate(Material.IRON, Sensitivity.XPORB).setUnlocalizedName("ss.lapis_pressure_plate");
+        UtilRegistry.registerNormalBlock(lapisPressurePlate, "LapisPressurePlate", "lapis_pressure_plate");
 
-		copperPressurePlate = new BlockSSPressurePlate(Material.IRON, Sensitivity.AGEABLE).setUnlocalizedName("ss.copper_pressure_plate");
-		UtilRegistry.registerNormalBlock(copperPressurePlate, "CopperPressurePlate", "copper_pressure_plate");
+        copperPressurePlate = new BlockSSPressurePlate(Material.IRON, Sensitivity.AGEABLE).setUnlocalizedName("ss.copper_pressure_plate");
+        UtilRegistry.registerNormalBlock(copperPressurePlate, "CopperPressurePlate", "copper_pressure_plate");
 
-		silverPressurePlate = new BlockSSPressurePlate(Material.IRON, Sensitivity.ENEMY).setUnlocalizedName("ss.silver_pressure_plate");
-		UtilRegistry.registerNormalBlock(silverPressurePlate, "SilverPressurePlate", "silver_pressure_plate");
+        silverPressurePlate = new BlockSSPressurePlate(Material.IRON, Sensitivity.ENEMY).setUnlocalizedName("ss.silver_pressure_plate");
+        UtilRegistry.registerNormalBlock(silverPressurePlate, "SilverPressurePlate", "silver_pressure_plate");
 
-		orichalcumPressurePlate = new BlockSSPressurePlate(Material.IRON, Sensitivity.PLAYER).setUnlocalizedName("ss.orichalcum_pressure_plate");
-		UtilRegistry.registerNormalBlock(orichalcumPressurePlate, "OrichalcumPressurePlate", "orichalcum_pressure_plate");
+        orichalcumPressurePlate = new BlockSSPressurePlate(Material.IRON, Sensitivity.PLAYER).setUnlocalizedName("ss.orichalcum_pressure_plate");
+        UtilRegistry.registerNormalBlock(orichalcumPressurePlate, "OrichalcumPressurePlate", "orichalcum_pressure_plate");
 
-		sanctuary = new BlockSanctuary().setUnlocalizedName("ss.sanctuary");
-		UtilRegistry.registerNormalBlock(sanctuary, "Sanctuary", "sanctuary");
+        sanctuary = new BlockSanctuary().setUnlocalizedName("ss.sanctuary");
+        UtilRegistry.registerNormalBlock(sanctuary, "Sanctuary", "sanctuary");
 
-		enderStone = new BlockEnderStoneMonument().setUnlocalizedName("ss.ender_stone");
-		UtilRegistry.registerNormalBlock(enderStone, "EnderStone", "ender_stone");
+        enderStone = new BlockEnderStoneMonument().setUnlocalizedName("ss.ender_stone");
+        UtilRegistry.registerNormalBlock(enderStone, "EnderStone", "ender_stone");
 
-		//林業
-		rubberSapling = new BlockSSSapling().setUnlocalizedName("ss.rubber_sapling");
-		UtilRegistry.registerNormalBlock(rubberSapling, "RubberSapling", "forestry/rubber_sapling");
+        enderStoneFoundation = new BlockSSBase(Material.ROCK).setUnlocalizedName("ss.ender_stone");
+        UtilRegistry.registerNormalBlock(enderStoneFoundation, "EnderStoneFoundation", "ender_stone_foundation");
 
-		rubberLog = new BlockSSLog().setUnlocalizedName("ss.rubber_log");
-		UtilRegistry.registerNormalBlock(rubberLog, "RubberLog", "forestry/rubber_log");
+        //林業
+        rubberSapling = new BlockSSSapling().setUnlocalizedName("ss.rubber_sapling");
+        UtilRegistry.registerNormalBlock(rubberSapling, "RubberSapling", "forestry/rubber_sapling");
 
-		rubberLeaves = new BlockSSLeaves(rubberSapling).setUnlocalizedName("ss.rubber_leaves");
-		UtilRegistry.registerNormalBlock(rubberLeaves, new ItemSSLeaves((BlockLeaves) rubberLeaves), "RubberLeaves", "forestry/rubber_leaves");
+        rubberLog = new BlockSSLog().setUnlocalizedName("ss.rubber_log");
+        UtilRegistry.registerNormalBlock(rubberLog, "RubberLog", "forestry/rubber_log");
 
-		spile = new BlockSpile().setUnlocalizedName("ss.spile").setUnlocalizedName("ss.spile");
-		UtilRegistry.registerNormalBlock(spile, "Spile", "spile");
+        rubberLeaves = new BlockSSLeaves(rubberSapling).setUnlocalizedName("ss.rubber_leaves");
+        UtilRegistry.registerNormalBlock(rubberLeaves, new ItemSSLeaves((BlockLeaves) rubberLeaves), "RubberLeaves", "forestry/rubber_leaves");
 
-		sapCauldron = new BlockSapCauldron(Sap.SAP).setUnlocalizedName("ss.sap_cauldron");
-		UtilRegistry.registerNormalBlock(sapCauldron, "SapCauldron", "cauldron/sap_cauldron");
+        spile = new BlockSpile().setUnlocalizedName("ss.spile").setUnlocalizedName("ss.spile");
+        UtilRegistry.registerNormalBlock(spile, "Spile", "spile");
 
-		rubberCauldron = new BlockSapCauldron(Sap.RUBBER).setUnlocalizedName("ss.rubber_cauldron");
-		UtilRegistry.registerNormalBlock(rubberCauldron, "RubberCauldron", "cauldron/rubber_cauldron");
+        sapCauldron = new BlockSapCauldron(Sap.SAP).setUnlocalizedName("ss.sap_cauldron");
+        UtilRegistry.registerNormalBlock(sapCauldron, "SapCauldron", "cauldron/sap_cauldron");
 
-		//鉱石
-		copperOre = new BlockSSOre(1).setUnlocalizedName("ss.copper_ore");
-		UtilRegistry.registerNormalBlock(copperOre, "CopperOre", "ore/copper_ore");
+        rubberCauldron = new BlockSapCauldron(Sap.RUBBER).setUnlocalizedName("ss.rubber_cauldron");
+        UtilRegistry.registerNormalBlock(rubberCauldron, "RubberCauldron", "cauldron/rubber_cauldron");
 
-		silverOre = new BlockSSOre(2).setUnlocalizedName("ss.silver_ore");
-		UtilRegistry.registerNormalBlock(silverOre, "SilverOre", "ore/silver_ore");
+        //鉱石
+        copperOre = new BlockSSOre(1).setUnlocalizedName("ss.copper_ore");
+        UtilRegistry.registerNormalBlock(copperOre, "CopperOre", "ore/copper_ore");
 
-		orichalcumOre = new BlockSSOre(3).setUnlocalizedName("ss.orichalcum_ore");
-		UtilRegistry.registerNormalBlock(orichalcumOre, "Orichalcum", "ore/orichalcum_ore");
+        silverOre = new BlockSSOre(2).setUnlocalizedName("ss.silver_ore");
+        UtilRegistry.registerNormalBlock(silverOre, "SilverOre", "ore/silver_ore");
 
-		//Large鉱石
-		coalLargeOre = new BlockLargeOre(SSItems.coalDust, Blocks.COAL_ORE, 1).setUnlocalizedName("ss.coal_large_ore");
-		UtilRegistry.registerNormalBlock(coalLargeOre, "CoalLargeOre", "ore/coal_largeore");
+        orichalcumOre = new BlockSSOre(3).setUnlocalizedName("ss.orichalcum_ore");
+        UtilRegistry.registerNormalBlock(orichalcumOre, "Orichalcum", "ore/orichalcum_ore");
 
-		ironLargeOre = new BlockLargeOre(SSItems.ironDust, Blocks.IRON_ORE, 2).setUnlocalizedName("ss.iron_large_ore");
-		UtilRegistry.registerNormalBlock(ironLargeOre, "IronLargeOre", "ore/iron_large_ore");
+        //Large鉱石
+        coalLargeOre = new BlockLargeOre(SSItems.coalDust, Blocks.COAL_ORE, 1).setUnlocalizedName("ss.coal_large_ore");
+        UtilRegistry.registerNormalBlock(coalLargeOre, "CoalLargeOre", "ore/coal_largeore");
 
-		goldLargeOre = new BlockLargeOre(SSItems.goldDust, Blocks.GOLD_ORE, 3).setUnlocalizedName("ss.gold_large_ore");
-		UtilRegistry.registerNormalBlock(goldLargeOre, "GoldLargeOre", "ore/gold_large_ore");
+        ironLargeOre = new BlockLargeOre(SSItems.ironDust, Blocks.IRON_ORE, 2).setUnlocalizedName("ss.iron_large_ore");
+        UtilRegistry.registerNormalBlock(ironLargeOre, "IronLargeOre", "ore/iron_large_ore");
 
-		///diamondLargeOre = new BlockLargeOre(Items.COAL, Blocks.DIAMOND_ORE, 1).setUnlocalizedName("ss.diamond_large_ore");
-		//UtilRegistry.registerNormalBlock(diamondLargeOre, "DiamondLargeOre", "ore/diamond_large_ore");
+        goldLargeOre = new BlockLargeOre(SSItems.goldDust, Blocks.GOLD_ORE, 3).setUnlocalizedName("ss.gold_large_ore");
+        UtilRegistry.registerNormalBlock(goldLargeOre, "GoldLargeOre", "ore/gold_large_ore");
 
-		copperLargeOre = new BlockLargeOre(SSItems.coalDust, SSBlocks.copperOre, 2).setUnlocalizedName("ss.copper_large_ore");
-		UtilRegistry.registerNormalBlock(copperLargeOre, "CopperLargeOre", "ore/copper_large_ore");
+        ///diamondLargeOre = new BlockLargeOre(Items.COAL, Blocks.DIAMOND_ORE, 1).setUnlocalizedName("ss.diamond_large_ore");
+        //UtilRegistry.registerNormalBlock(diamondLargeOre, "DiamondLargeOre", "ore/diamond_large_ore");
 
-		silverLargeOre = new BlockLargeOre(SSItems.silverDust, SSBlocks.silverOre, 3).setUnlocalizedName("ss.silver_large_ore");
-		UtilRegistry.registerNormalBlock(silverLargeOre, "SilverLargeOre", "ore/silver_large_ore");
+        copperLargeOre = new BlockLargeOre(SSItems.coalDust, SSBlocks.copperOre, 2).setUnlocalizedName("ss.copper_large_ore");
+        UtilRegistry.registerNormalBlock(copperLargeOre, "CopperLargeOre", "ore/copper_large_ore");
 
-		//orichalcumLargeOre = new BlockLargeOre(SSItems.orichalcumGem, SSBlocks.orichalcumOre, 1).setUnlocalizedName("ss.orichalcum_large_ore");
-		//UtilRegistry.registerNormalBlock(orichalcumLargeOre, "orichalcum_large_ore", "ore/orichalcum_large_ore");
+        silverLargeOre = new BlockLargeOre(SSItems.silverDust, SSBlocks.silverOre, 3).setUnlocalizedName("ss.silver_large_ore");
+        UtilRegistry.registerNormalBlock(silverLargeOre, "SilverLargeOre", "ore/silver_large_ore");
 
-		copperBlock = new BlockSSBase(Material.IRON).setUnlocalizedName("ss.copper_block");
-		UtilRegistry.registerNormalBlock(copperBlock, "CopperBlock", "ore/copper_block");
+        //orichalcumLargeOre = new BlockLargeOre(SSItems.orichalcumGem, SSBlocks.orichalcumOre, 1).setUnlocalizedName("ss.orichalcum_large_ore");
+        //UtilRegistry.registerNormalBlock(orichalcumLargeOre, "orichalcum_large_ore", "ore/orichalcum_large_ore");
 
-		silverBlock = new BlockSSBase(Material.IRON).setUnlocalizedName("ss.silver_block");
-		UtilRegistry.registerNormalBlock(silverBlock, "SilverBlock", "ore/silver_block");
+        copperBlock = new BlockSSBase(Material.IRON).setUnlocalizedName("ss.copper_block");
+        UtilRegistry.registerNormalBlock(copperBlock, "CopperBlock", "ore/copper_block");
 
-		orichalcumBlock = new BlockSSBase(Material.IRON).setUnlocalizedName("ss.orichalcum_block");
-		UtilRegistry.registerNormalBlock(orichalcumBlock, "OrichalcumBlock", "ore/orichalcum_block");
+        silverBlock = new BlockSSBase(Material.IRON).setUnlocalizedName("ss.silver_block");
+        UtilRegistry.registerNormalBlock(silverBlock, "SilverBlock", "ore/silver_block");
 
-	}
+        orichalcumBlock = new BlockSSBase(Material.IRON).setUnlocalizedName("ss.orichalcum_block");
+        UtilRegistry.registerNormalBlock(orichalcumBlock, "OrichalcumBlock", "ore/orichalcum_block");
+
+    }
 
 }
