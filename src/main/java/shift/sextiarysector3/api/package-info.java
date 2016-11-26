11d9d477679d10f36218fca8c5d063d5f966bdff
shift02 <shift02ss@gmail.com>
2016-11-26 21:38:17 +0900

    追加：APIの設定

---
+++ src/main/java/shift/sextiarysector3/api/package-info.java
@@ -0,0 +1,4 @@
+@API(apiVersion = "1.0.0", owner = shift.sextiarysector3.api.SextiarySectorAPI.MODID, provides = "sextiarysector3|core")
+package shift.sextiarysector3.api;
+
+import net.minecraftforge.fml.common.API;
