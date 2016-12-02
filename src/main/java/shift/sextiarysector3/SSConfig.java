77f4b9664ed76cc31c6249bbe4dd5876d5da770f
shift02 <shift02ss@gmail.com>
2016-12-02 22:17:52 +0900

    追加：葉っぱの消去をConfigで変更できるように

--- src/main/java/shift/sextiarysector3/SSConfig.java
+++ src/main/java/shift/sextiarysector3/SSConfig.java
@@ -1,10 +1,15 @@
 package shift.sextiarysector3;
 
 import java.io.File;
+import java.util.List;
 
 import org.apache.logging.log4j.Level;
 
+import com.google.common.collect.Lists;
+
+import net.minecraft.util.text.translation.I18n;
 import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Loader;
 
@@ -13,6 +18,9 @@ public class SSConfig {
     public static Configuration config;
 
     public static final String SS_LANG = "config.ss.";
+
+    //General
+    public static boolean fastDecayLeaves;
 
     public static void initConfig() {
 
@@ -45,6 +53,22 @@ public class SSConfig {
     public static void syncConfig() {
 
         initConfig();
+
+        String category = Configuration.CATEGORY_GENERAL;
+
+        Property prop;
+        List<String> propOrder = Lists.newArrayList();
+
+        //Fast Decay Leaves - 葉の高速消滅
+        prop = config.get(category, "fast_decay_leaves", true);
+        prop.setLanguageKey(SS_LANG + category + "." + prop.getName());
+        prop.setComment(I18n.translateToLocal(prop.getLanguageKey() + ".tooltip"));
+        prop.setComment(prop.getComment() + " [default: " + prop.getDefault() + "]");
+        propOrder.add(prop.getName());
+        fastDecayLeaves = prop.getBoolean(fastDecayLeaves);
+
+        config.setCategoryLanguageKey(category, SS_LANG + category);
+        config.setCategoryPropertyOrder(category, propOrder);
 
         if (config.hasChanged()) {
 
--- src/main/java/shift/sextiarysector3/event/CommonEventHandler.java
+++ src/main/java/shift/sextiarysector3/event/CommonEventHandler.java
@@ -9,53 +9,54 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import shift.sextiarysector3.SSConfig;
 
 public class CommonEventHandler {
 
-	//葉っぱ
-	@SubscribeEvent
-	public void DecayEvent(BlockEvent.HarvestDropsEvent event) {
+    //葉っぱ
+    @SubscribeEvent
+    public void DecayEvent(BlockEvent.HarvestDropsEvent event) {
 
-		if (event.isSilkTouching()) return;
+        if (event.isSilkTouching()) return;
 
-		if (!(event.getState().getBlock() instanceof BlockLeaves)) return;
+        if (!(event.getState().getBlock() instanceof BlockLeaves)) return;
 
-		if (event.getHarvester() != null && event.getHarvester().getActiveItemStack() != null
-				&& event.getHarvester().getActiveItemStack().getItem() instanceof ItemShears)
-			return;
+        if (event.getHarvester() != null && event.getHarvester().getActiveItemStack() != null
+                && event.getHarvester().getActiveItemStack().getItem() instanceof ItemShears)
+            return;
 
-		if (event.getWorld().rand.nextBoolean()) event.getDrops().add(new ItemStack(Items.STICK, event.getWorld().rand.nextInt(1) + 1));
-		//if (event.getWorld().rand.nextInt(3) == 0) event.getDrops().add(new ItemStack(SSItems.leaf, event.world.rand.nextInt(2) + 1));
+        if (event.getWorld().rand.nextBoolean()) event.getDrops().add(new ItemStack(Items.STICK, event.getWorld().rand.nextInt(1) + 1));
+        //if (event.getWorld().rand.nextInt(3) == 0) event.getDrops().add(new ItemStack(SSItems.leaf, event.world.rand.nextInt(2) + 1));
 
-		//if (!SSConfig.fastDecayLeaves) return;
+        if (!SSConfig.fastDecayLeaves) return;
 
-		byte b0 = 2;
-		int i1 = b0 + 1;
+        byte b0 = 2;
+        int i1 = b0 + 1;
 
-		//if (event.getWorld().checkChunksExist(
-		//		event.getPos().getX() - i1, event.getPos().getY() - i1, event.getPos().getZ() - i1, event.getPos().getX() + i1, event.getPos().getY() + i1,
-		//		event.getPos().getZ() + i1)) {
-		for (int j1 = -b0; j1 <= b0; ++j1) {
-			for (int k1 = -b0; k1 <= b0; ++k1) {
-				for (int l1 = -b0; l1 <= b0; ++l1) {
+        //if (event.getWorld().checkChunksExist(
+        //		event.getPos().getX() - i1, event.getPos().getY() - i1, event.getPos().getZ() - i1, event.getPos().getX() + i1, event.getPos().getY() + i1,
+        //		event.getPos().getZ() + i1)) {
+        for (int j1 = -b0; j1 <= b0; ++j1) {
+            for (int k1 = -b0; k1 <= b0; ++k1) {
+                for (int l1 = -b0; l1 <= b0; ++l1) {
 
-					BlockPos pos = event.getPos().add(j1, k1, l1);
-					IBlockState state = event.getWorld().getBlockState(pos);
-					Block block = state.getBlock();
+                    BlockPos pos = event.getPos().add(j1, k1, l1);
+                    IBlockState state = event.getWorld().getBlockState(pos);
+                    Block block = state.getBlock();
 
-					if (block.isLeaves(state, event.getWorld(), pos)) {
-						//block.updateTick(event.world, event.x + j1, event.y + k1, event.z + l1, event.world.rand);
-						event.getWorld().scheduleBlockUpdate(pos, block, 20 + event.getWorld().rand.nextInt(8), 1);
-					}
-				}
-			}
-		}
-		//}
+                    if (block.isLeaves(state, event.getWorld(), pos)) {
+                        //block.updateTick(event.world, event.x + j1, event.y + k1, event.z + l1, event.world.rand);
+                        event.getWorld().scheduleBlockUpdate(pos, block, 20 + event.getWorld().rand.nextInt(8), 1);
+                    }
+                }
+            }
+        }
+        //}
 
-		//for (int i = 0; i < 2; i++) {
-		//	this.updateLeavesTick(event);
-		//}
+        //for (int i = 0; i < 2; i++) {
+        //	this.updateLeavesTick(event);
+        //}
 
-	}
+    }
 
 }
