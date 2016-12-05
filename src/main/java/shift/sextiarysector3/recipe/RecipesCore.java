0bbe7bab2c0fcb494e6d8160db7e183c044088b4
shift02 <shift02ss@gmail.com>
2016-12-05 22:34:05 +0900

    追加：レシピ

--- src/main/java/shift/sextiarysector3/recipe/RecipesCore.java
+++ src/main/java/shift/sextiarysector3/recipe/RecipesCore.java
@@ -9,35 +9,43 @@ import shift.sextiarysector3.SSItems;
 
 public class RecipesCore {
 
-	public static void addRecipes(CraftingManager p_77608_1_) {
+    public static void addRecipes(CraftingManager p_77608_1_) {
 
-		//Shield
-		p_77608_1_.getRecipeList().add(new ShapedOreRecipe(new ItemStack(SSItems.plasticShield, 1),
-				new Object[] {
-						"x x", "xxx", " x ",
-						'x', "plastic"
-				}));
+        //Shield
+        p_77608_1_.getRecipeList().add(new ShapedOreRecipe(new ItemStack(SSItems.plasticShield, 1),
+                new Object[] {
+                        "x x", "xxx", " x ",
+                        'x', "plastic"
+                }));
 
-		//鉱石 インゴット--プレート
-		Object[] oIngot = new Object[] { "gemLapis", "ingotCopper", "ingotSilver", "gemOrichalcum" };
-		Block[] pressurePlate = new Block[] { SSBlocks.lapisPressurePlate, SSBlocks.copperPressurePlate, SSBlocks.silverPressurePlate,
-				SSBlocks.orichalcumPressurePlate };
-		for (int i = 0; i < oIngot.length; i++) {
-			p_77608_1_.getRecipeList().add(new ShapedOreRecipe(new ItemStack(pressurePlate[i], 1),
-					new Object[] { "xx",
-							Character.valueOf('x'), oIngot[i],
-					}));
-		}
+        //鉱石 インゴット--プレート
+        Object[] oIngot = new Object[] { "gemLapis", "ingotCopper", "ingotSilver", "gemOrichalcum" };
+        Block[] pressurePlate = new Block[] { SSBlocks.lapisPressurePlate, SSBlocks.copperPressurePlate, SSBlocks.silverPressurePlate,
+                SSBlocks.orichalcumPressurePlate };
+        for (int i = 0; i < oIngot.length; i++) {
+            p_77608_1_.getRecipeList().add(new ShapedOreRecipe(new ItemStack(pressurePlate[i], 1),
+                    new Object[] { "xx",
+                            Character.valueOf('x'), oIngot[i],
+                    }));
+        }
 
-		//スパイル
-		p_77608_1_.getRecipeList().add(new ShapedOreRecipe(new ItemStack(SSBlocks.sanctuary, 2),
-				new Object[] {
-						"xyx", "xzx", "xxx",
-						'x', "stone",
-						'y', "ingotSilver",
-						'z', "blockRedstone"
-				}));
+        //サンクチュアリ
+        p_77608_1_.getRecipeList().add(new ShapedOreRecipe(new ItemStack(SSBlocks.sanctuary, 2),
+                new Object[] {
+                        "xyx", "xzx", "xxx",
+                        'x', "stone",
+                        'y', "ingotSilver",
+                        'z', "blockRedstone"
+                }));
 
-	}
+        //深度計
+        p_77608_1_.getRecipeList().add(new ShapedOreRecipe(new ItemStack(SSItems.depthMeter, 1),
+                new Object[] {
+                        " x ", "xyx", " x ",
+                        'x', "ingotSilver",
+                        'y', "dustRedstone"
+                }));
+
+    }
 
 }
