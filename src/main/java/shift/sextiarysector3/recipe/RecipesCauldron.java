e95fe75bad5a43c9b662241fc21d1594a6c072ff
shift02 <shift02ss@gmail.com>
2016-12-16 21:59:43 +0900

    追加：大釜のレシピ

--- src/main/java/shift/sextiarysector3/recipe/RecipesCauldron.java
+++ src/main/java/shift/sextiarysector3/recipe/RecipesCauldron.java
@@ -6,6 +6,8 @@ import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
+import shift.sextiarysector3.SSFluids;
+import shift.sextiarysector3.SSItems;
 import shift.sextiarysector3.recipe.template.RecipeCauldron;
 
 public class RecipesCauldron {
@@ -13,6 +15,10 @@ public class RecipesCauldron {
     public static void addRecipes(List<RecipeCauldron> list) {
 
         list.add(new RecipeCauldron(new ItemStack(Items.GLASS_BOTTLE), new FluidStack(FluidRegistry.WATER, 300), new ItemStack(Items.POTIONITEM)));
+
+        list.add(new RecipeCauldron(new ItemStack(Items.GLASS_BOTTLE), new FluidStack(SSFluids.rubber, 300), new ItemStack(SSItems.rubberBottle)));
+
+        list.add(new RecipeCauldron(new ItemStack(Items.GLASS_BOTTLE), new FluidStack(SSFluids.sap, 300), new ItemStack(SSItems.sapBottle)));
 
     }
 
