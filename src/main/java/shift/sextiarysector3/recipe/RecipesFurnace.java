a1b18117b8b0c8fb13dc331db80b28c02bb5c808
shift02 <shift02ss@gmail.com>
2017-01-01 22:00:02 +0900

    追加：ゴムのレシピ

--- src/main/java/shift/sextiarysector3/recipe/RecipesFurnace.java
+++ src/main/java/shift/sextiarysector3/recipe/RecipesFurnace.java
@@ -8,20 +8,21 @@ import shift.sextiarysector3.SSItems;
 
 public class RecipesFurnace {
 
-	public static void addRecipes() {
+    public static void addRecipes() {
 
-		GameRegistry.addSmelting(new ItemStack(Items.REDSTONE), new ItemStack(SSItems.bluestone), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(Items.REDSTONE), new ItemStack(SSItems.bluestone), 0.1f);
 
-		GameRegistry.addSmelting(new ItemStack(SSItems.sapBottle), new ItemStack(SSItems.plastic), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(SSItems.sapBottle), new ItemStack(SSItems.plastic), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(SSItems.rubberBottle), new ItemStack(SSItems.rubber), 0.1f);
 
-		GameRegistry.addSmelting(new ItemStack(SSBlocks.copperOre), new ItemStack(SSItems.copperIngot), 0.1f);
-		GameRegistry.addSmelting(new ItemStack(SSBlocks.silverOre), new ItemStack(SSItems.silverIngot), 0.1f);
-		GameRegistry.addSmelting(new ItemStack(SSBlocks.orichalcumOre), new ItemStack(SSItems.orichalcumGem), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(SSBlocks.copperOre), new ItemStack(SSItems.copperIngot), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(SSBlocks.silverOre), new ItemStack(SSItems.silverIngot), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(SSBlocks.orichalcumOre), new ItemStack(SSItems.orichalcumGem), 0.1f);
 
-		GameRegistry.addSmelting(new ItemStack(SSItems.ironDust), new ItemStack(Items.IRON_INGOT), 0.1f);
-		GameRegistry.addSmelting(new ItemStack(SSItems.goldDust), new ItemStack(Items.GOLD_INGOT), 0.1f);
-		GameRegistry.addSmelting(new ItemStack(SSItems.copperDust), new ItemStack(SSItems.copperIngot), 0.1f);
-		GameRegistry.addSmelting(new ItemStack(SSItems.silverDust), new ItemStack(SSItems.silverIngot), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(SSItems.ironDust), new ItemStack(Items.IRON_INGOT), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(SSItems.goldDust), new ItemStack(Items.GOLD_INGOT), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(SSItems.copperDust), new ItemStack(SSItems.copperIngot), 0.1f);
+        GameRegistry.addSmelting(new ItemStack(SSItems.silverDust), new ItemStack(SSItems.silverIngot), 0.1f);
 
-	}
+    }
 }
