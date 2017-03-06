f31f33806ebf9905756385d96aded4846fbd95f3
shift02 <shift02ss@gmail.com>
2017-03-06 17:52:03 +0900

    追加：６方向のブロック

--- src/main/java/shift/sextiarysector3/block/BlockShaft.java
+++ src/main/java/shift/sextiarysector3/block/BlockShaft.java
@@ -1,32 +1,31 @@
 package shift.sextiarysector3.block;
 
-import net.minecraft.block.BlockDirectional;
-import net.minecraft.block.BlockPistonBase;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyDirection;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.Mirror;
-import net.minecraft.util.Rotation;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import shift.sextiarysector3.tileentity.TileEntityShaft;
 
-public class BlockShaft extends BlockSSBase implements ITileEntityProvider {
-
-    public static final PropertyDirection FACING = BlockDirectional.FACING;
+public class BlockShaft extends BlockSSDirectional implements ITileEntityProvider {
 
     public BlockShaft(Material materialIn) {
         super(materialIn);
         this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
     }
+
+    /*
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, @Nullable ItemStack heldItem, EnumFacing side, float hitX, float hitY,
+            float hitZ) {
+    
+        if (worldIn.isRemote) return true;
+    
+        ((TileEntityShaft) worldIn.getTileEntity(pos)).speed += 5;
+        if (((TileEntityShaft) worldIn.getTileEntity(pos)).speed > 50) ((TileEntityShaft) worldIn.getTileEntity(pos)).speed = 0;
+    
+        return true;
+    }*/
 
     public boolean isOpaqueCube(IBlockState state) {
         return false;
@@ -36,78 +35,8 @@ public class BlockShaft extends BlockSSBase implements ITileEntityProvider {
         return false;
     }
 
-    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
-        super.onBlockAdded(worldIn, pos, state);
-        this.setDefaultDirection(worldIn, pos, state);
-    }
-
-    private void setDefaultDirection(World worldIn, BlockPos pos, IBlockState state) {
-
-        if (!worldIn.isRemote) {
-            EnumFacing enumfacing = (EnumFacing) state.getValue(FACING);
-            boolean flag = worldIn.getBlockState(pos.north()).isFullBlock();
-            boolean flag1 = worldIn.getBlockState(pos.south()).isFullBlock();
-
-            if (enumfacing == EnumFacing.NORTH && flag && !flag1) {
-                enumfacing = EnumFacing.SOUTH;
-            } else if (enumfacing == EnumFacing.SOUTH && flag1 && !flag) {
-                enumfacing = EnumFacing.NORTH;
-            } else {
-                boolean flag2 = worldIn.getBlockState(pos.west()).isFullBlock();
-                boolean flag3 = worldIn.getBlockState(pos.east()).isFullBlock();
-
-                if (enumfacing == EnumFacing.WEST && flag2 && !flag3) {
-                    enumfacing = EnumFacing.EAST;
-                } else if (enumfacing == EnumFacing.EAST && flag3 && !flag2) {
-                    enumfacing = EnumFacing.WEST;
-                }
-            }
-            worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing), 2);
-        }
-
-    }
-
-    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
-        return this.getDefaultState().withProperty(FACING, BlockPistonBase.getFacingFromEntity(pos, placer));
-    }
-
-    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
-        worldIn.setBlockState(pos, state.withProperty(FACING, BlockPistonBase.getFacingFromEntity(pos, placer)), 2);
-
-    }
-
     public TileEntity createNewTileEntity(World worldIn, int meta) {
         return new TileEntityShaft();
-    }
-
-    public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta & 7));
-    }
-
-    public EnumBlockRenderType getRenderType(IBlockState state) {
-        return EnumBlockRenderType.ENTITYBLOCK_ANIMATED;
-    }
-
-    /**
-     * Convert the BlockState into the correct metadata value
-     */
-    public int getMetaFromState(IBlockState state) {
-        int i = 0;
-        i = i | ((EnumFacing) state.getValue(FACING)).getIndex();
-
-        return i;
-    }
-
-    public IBlockState withRotation(IBlockState state, Rotation rot) {
-        return state.withProperty(FACING, rot.rotate((EnumFacing) state.getValue(FACING)));
-    }
-
-    public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
-        return state.withRotation(mirrorIn.toRotation((EnumFacing) state.getValue(FACING)));
-    }
-
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, new IProperty[] { FACING });
     }
 
 }
