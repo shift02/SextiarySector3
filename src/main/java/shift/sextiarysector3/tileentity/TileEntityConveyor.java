add19ab013bedb7d3b409cf7ace26ef045393134
shift02 <shift02ss@gmail.com>
2017-03-18 22:15:15 +0900

    追加：Entityの移動処理

--- src/main/java/shift/sextiarysector3/tileentity/TileEntityConveyor.java
+++ src/main/java/shift/sextiarysector3/tileentity/TileEntityConveyor.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
@@ -16,8 +17,13 @@ public class TileEntityConveyor extends TileEntity implements ITickable {
 
     public long actionTime = 0;
 
+    public final String MOVE_NOW = "move_now";
+
     @Override
     public void update() {
+
+        actionTime = worldObj.getWorldInfo().getWorldTime();
+
         if (worldObj.isRemote) {
             this.updateClient();
         } else {
@@ -46,12 +52,31 @@ public class TileEntityConveyor extends TileEntity implements ITickable {
 
         EnumFacing f = worldObj.getBlockState(getPos()).getValue(BlockConveyor.FACING);
 
+        float ani = (1 / 16.0f) / 2.0f;
+
         for (Entity e : list) {
             if (e instanceof EntityPlayer) continue;
-            e.motionX += (f.getFrontOffsetX() / 40.0f);
-            e.motionY += (f.getFrontOffsetY() / 40.0f);
-            e.motionZ += (f.getFrontOffsetZ() / 40.0f);
+
+            if (this.isMoveNow(e)) continue;
+            //e.motionX += (f.getFrontOffsetX() / 46.0f);
+            //e.motionY += (f.getFrontOffsetY() / 46.0f);
+            //e.motionZ += (f.getFrontOffsetZ() / 46.0f);
+            e.setPosition(e.posX + f.getFrontOffsetX() * ani, e.posY + f.getFrontOffsetY() * ani, e.posZ + f.getFrontOffsetZ() * ani);
+
+            e.getEntityData().setLong(MOVE_NOW, actionTime);
+
         }
+
+    }
+
+    public boolean isMoveNow(Entity e) {
+
+        NBTTagCompound nbt = e.getEntityData();
+
+        if (!nbt.hasKey(MOVE_NOW)) return false;
+        if (nbt.getLong(MOVE_NOW) != this.actionTime) return false;
+
+        return true;
 
     }
 
