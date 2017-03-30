6bc3e5656fb4b88988a662f02aff4d735b5e6304
shift02 <shift02ss@gmail.com>
2017-03-30 23:51:23 +0900

    追加：風車

---
+++ src/main/java/shift/sextiarysector3/block/BlockSmallWindmill.java
@@ -0,0 +1,11 @@
+package shift.sextiarysector3.block;
+
+import net.minecraft.block.material.Material;
+
+public class BlockSmallWindmill extends BlockSSBase {
+
+    public BlockSmallWindmill() {
+        super(Material.WOOD);
+    }
+
+}
