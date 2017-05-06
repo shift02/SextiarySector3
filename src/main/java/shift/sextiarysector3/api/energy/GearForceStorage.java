a92ed43fc8a8a7442e472c9ce341d7c77ee077c5
shift02 <shift02ss@gmail.com>
2017-05-06 22:31:44 +0900

    追加：エネルギーの基礎クラス

--- src/main/java/shift/sextiarysector3/api/energy/GearForceStorage.java
+++ src/main/java/shift/sextiarysector3/api/energy/GearForceStorage.java
@@ -2,49 +2,86 @@ package shift.sextiarysector3.api.energy;
 
 public class GearForceStorage implements IGearForceStorage {
 
+    protected int power;
+    protected int powerCapacity;
+    protected int speed;
+    protected int speedCapacity;
+
+    protected boolean isReceive;
+    protected boolean isExtract;
+
+    public GearForceStorage(int power, int capacity) {
+        this(power, capacity, true, true);
+    }
+
+    public GearForceStorage(int power, int capacity, boolean isReceive, boolean isExtract) {
+        this.powerCapacity = power;
+        this.speedCapacity = capacity;
+        this.isReceive = isReceive;
+        this.isExtract = isExtract;
+    }
+
     @Override
     public int receiveSpeed(int power, int maxReceive, boolean simulate) {
-        return 0;
+
+        if (!canReceive())
+            return 0;
+
+        if (power != powerCapacity) return 0;
+
+        int energyReceived = Math.min(maxReceive, speedCapacity);
+        if (!simulate) speed = energyReceived;
+        return energyReceived;
+
     }
 
     @Override
     public int extractSpeed(int power, int maxExtract, boolean simulate) {
-        return 0;
+
+        if (!canExtract())
+            return 0;
+
+        if (power != powerCapacity) return 0;
+
+        int energyExtracted = Math.min(speed, maxExtract);
+        if (!simulate) speed = 0;
+        return energyExtracted;
+
     }
 
     @Override
     public int getSpeedStored() {
-        return 0;
+        return speed;
     }
 
     @Override
     public int getMaxSpeedStored() {
-        return 0;
+        return speedCapacity;
     }
 
     @Override
     public int getMinPowerWorked() {
-        return 0;
+        return powerCapacity;
     }
 
     @Override
     public int getPowerStored() {
-        return 0;
+        return power;
     }
 
     @Override
     public int getMaxPowerWorked() {
-        return 0;
+        return powerCapacity;
     }
 
     @Override
     public boolean canExtract() {
-        return false;
+        return this.isExtract;
     }
 
     @Override
     public boolean canReceive() {
-        return false;
+        return this.isReceive;
     }
 
 }
