a91ceff3e8f5e8760fd1386c31a08336991a771c
shift02 <shift02ss@gmail.com>
2017-06-09 20:41:15 +0900

    更新：バージョン

--- src/main/java/shift/sextiarysector3/SextiarySector3.java
+++ src/main/java/shift/sextiarysector3/SextiarySector3.java
@@ -30,7 +30,7 @@ import shift.sextiarysector3.util.UtilRegistry;
 public class SextiarySector3 {
 
     public static final String MODID = "sextiarysector3";
-    public static final String VERSION = "1.3.3";
+    public static final String VERSION = "1.3.4";
 
     public static final String UPDATE_JSON = "https://shift02.github.io/SextiarySector3/Update.json";
 
