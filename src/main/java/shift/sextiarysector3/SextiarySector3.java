a77ea6a723e6e85ad23fbbd1145ade246c9561c7
shift02 <shift02ss@gmail.com>
2017-06-09 18:50:29 +0900

    追加：Capの登録

--- src/main/java/shift/sextiarysector3/SextiarySector3.java
+++ src/main/java/shift/sextiarysector3/SextiarySector3.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import shift.sextiarysector3.api.SextiarySectorAPI;
+import shift.sextiarysector3.api.energy.CapabilityGearForce;
 import shift.sextiarysector3.api.energy.CapabilityGearForceHandler;
 import shift.sextiarysector3.api.industry.CapabilityShaftHandler;
 import shift.sextiarysector3.module.IModule;
@@ -88,6 +89,7 @@ public class SextiarySector3 {
         CapabilityShaftHandler.register();
         CapabilityGFShaftHandler.register();
         CapabilityGearForceHandler.register();
+        CapabilityGearForce.register();
 
     }
 
