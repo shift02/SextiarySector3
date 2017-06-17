b36f4825e5a24eb71eea129edaefd44dc34c6a6d
shift02 <shift02ss@gmail.com>
2017-06-17 21:38:17 +0900

    追加：フルーツ用の葉っぱ

---
+++ src/main/java/shift/sextiarysector3/block/BlockSSFruitLeaves.java
@@ -0,0 +1,70 @@
+package shift.sextiarysector3.block;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.IGrowable;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+
+public class BlockSSFruitLeaves extends BlockSSLeaves implements IGrowable {
+
+    public BlockSSFruitLeaves(Block sapling) {
+        super(sapling);
+
+    }
+
+    @Override
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+        super.updateTick(worldIn, pos, state, rand);
+
+        if (worldIn.getLightFromNeighbors(pos.up()) >= 9) {
+
+            int i = this.getAge(state);
+
+            if (i < this.getMaxAge()) {
+                float f = 25.0f;//getGrowthChance(this, worldIn, pos);
+
+                if (net.minecraftforge.common.ForgeHooks.onCropsGrowPre(worldIn, pos, state, rand.nextInt((int) (25.0F / f) + 1) == 0)) {
+                    worldIn.setBlockState(pos, state.withProperty(AGE, i + 1), 2);
+                    net.minecraftforge.common.ForgeHooks.onCropsGrowPost(worldIn, pos, state, worldIn.getBlockState(pos));
+                }
+            }
+
+        }
+
+    }
+
+    @Override
+    public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+        int i = this.getAge(state) + this.getBonemealAgeIncrease(worldIn);
+        int j = this.getMaxAge();
+
+        if (i > j) {
+            i = j;
+        }
+
+        worldIn.setBlockState(pos, state.withProperty(AGE, i), 2);
+    }
+
+    protected int getBonemealAgeIncrease(World worldIn) {
+        return MathHelper.getRandomIntegerInRange(worldIn.rand, 2, 5);
+    }
+
+    @Override
+    public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
+        return true;
+    }
+
+    @Override
+    public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+        return true;
+    }
+
+    public int getMaxAge() {
+        return 3;
+    }
+
+}
