9a675c1d70803a8cff0709fcf83cc2d36540743d
shift02 <shift02ss@gmail.com>
2017-06-24 22:27:26 +0900

    追加：輝く花のレシピ

--- src/main/java/shift/sextiarysector3/recipe/RecipesPharmacy.java
+++ src/main/java/shift/sextiarysector3/recipe/RecipesPharmacy.java
@@ -1,7 +1,12 @@
 package shift.sextiarysector3.recipe;
 
+import net.minecraft.block.BlockDoublePlant;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import shift.sextiarysector3.SSItems;
 import shift.sextiarysector3.recipe.template.SSBrewingRecipe;
 
 public class RecipesPharmacy {
@@ -9,6 +14,13 @@ public class RecipesPharmacy {
     public static void addRecipes(CraftingManager p_77608_1_) {
 
         BrewingRecipeRegistry.addRecipe(new SSBrewingRecipe());
+
+        p_77608_1_.getRecipeList().add(new ShapedOreRecipe(new ItemStack(SSItems.shiningFlower, 1),
+                new Object[] {
+                        "xxx", "xyx", "xxx",
+                        'y', new ItemStack(Blocks.DOUBLE_PLANT, 1, BlockDoublePlant.EnumPlantType.SUNFLOWER.getMeta()),
+                        'x', "nuggetSilver"
+                }));
 
     }
 
