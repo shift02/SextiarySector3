a25ad0770cf33df1f504a192432a117f608fc6ba
shift02 <shift02ss@gmail.com>
2017-07-31 23:35:51 +0900

    追加：食べ物

---
+++ src/main/java/shift/sextiarysector3/item/ItemSSFood.java
@@ -0,0 +1,11 @@
+package shift.sextiarysector3.item;
+
+import net.minecraft.item.ItemFood;
+
+public class ItemSSFood extends ItemFood {
+
+    public ItemSSFood(int amount, float saturation, boolean isWolfFood) {
+        super(amount, saturation, isWolfFood);
+    }
+
+}
